7714d8b16a57d9e3b7589fc12f5250402b4e9ff0
Aimee <AimeeKnight83@gmail.com>
2014-07-02 11:41:30 -0500

    initial commit - functional JS

---
+++ FunctionalJavaScript/program.js
@@ -0,0 +1,44 @@
+
+function upperCaser(input) {
+  return input.toUpperCase();
+}
+
+function repeat(operation, num) {
+  if (num <= 0) return
+  operation()
+  return repeat(operation, --num)
+}
+
+function doubleAll(numbers){
+  return numbers.map(function(num){
+    return num * 2;
+  });
+}
+
+function getShortMessage(messages){
+  return messages.filter(function(x){
+    return x.message.length < 50
+  }).map(function(x){
+    return x.message
+  })
+}
+
+function checkUsersValid(goodUsers){
+  return function(submittedUsers){
+    return submittedUsers.every(function(submittedUser){
+      return goodUsers.some(function(user){
+        return user.id === submittedUser.id
+      });
+    });
+  };
+}
+
+function countWords(inputWords){
+  return inputWords.reduce(function(counter, word) {
+    // increment counter key value if it's already present
+    counter[word] = ++ counter[word] || 1
+    return counter;
+  }, {});
+}
+
+module.exports = countWords
