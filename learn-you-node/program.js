5c10ddb58985b3036073aa2d93adb4b90a88681d
Aimee <AimeeKnight83@gmail.com>
2014-07-25 15:22:25 -0400

    finish learnyounode

--- learn-you-node/program.js
+++ learn-you-node/program.js
@@ -116,7 +116,7 @@ http.get(process.argv[2], function(response){
 */
 
 // 10
-
+/*
 var net = require("net");
 var strftime = require("strftime");
 
@@ -132,8 +132,74 @@ var server = net.createServer(function(socket){
   socket.end();
 });
 server.listen(process.argv[2]);
-
+*/
 
 // 11
+/*
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response){
+  var readStream = fs.createReadStream(process.argv[3]);
+  readStream.on('open', function(){
+    readStream.pipe(response);
+  });
+});
+server.listen(process.argv[2]);
+*/
+
+//12
+/*
+var http = require('http');
+var map = require('through2-map');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response){
+
+  if (request.method == "POST") {
+    request.pipe(map(function (chunk){
+      return chunk.toString().toUpperCase();
+    })).pipe(response)
+  }
+
+});
+server.listen(process.argv[2]);
+*/
+
+// 13
+/*
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(request, response){
+
+  if(request.url.indexOf('/api/parsetime') != -1){
+    var queryData = url.parse(request.url, true).query;
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    var current = new Date(queryData.iso);
+    response.end(JSON.stringify({
+      "hour": current.getHours(),
+      "minute": current.getMinutes(), 
+      "second": current.getSeconds(),
+    }));
+  }
+
+  if(request.url.indexOf('/api/unixtime') != -1){
+    var queryData2 = url.parse(request.url, true).query;
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    var unixTime = queryData2.iso;
+    var current = new Date(unixTime);
+    response.end(JSON.stringify({ "unixtime": current.getTime() }));
+  }
+
+});
+server.listen(process.argv[2]);
+*/
+
+
+
+
 
 
