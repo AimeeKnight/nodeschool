845d3f486a018f722a49363ba00d921f17ccd440
AimeeKnight <aimeemarieknight@gmail.com>
2014-09-14 19:27:36 -0400

    update nodeschool functional JS exercises

---
+++ program.js
@@ -0,0 +1,56 @@
+
+function upperCaser(input) {
+  return input.toUpperCase();
+}
+
+function repeat(operation, num) {
+  if (num <= 0) return
+  operation()
+  return repeat(operation, --num)
+}
+
+function doubleAll(numbers){
+  return numbers.map(function(num){
+    return num * 2;
+  });
+}
+
+function getShortMessage(messages){
+  return messages.filter(function(x){
+    return x.message.length < 50
+  }).map(function(x){
+    return x.message
+  })
+}
+
+function checkUsersValid(goodUsers){
+  return function(submittedUsers){
+    return submittedUsers.every(function(submittedUser){
+      return goodUsers.some(function(user){
+        return user.id === submittedUser.id
+      });
+    });
+  };
+}
+
+function countWords(inputWords){
+  return inputWords.reduce(function(counter, word) {
+    // increment counter key value if it's already present
+    counter[word] = ++ counter[word] || 1
+    return counter;
+  }, {});
+}
+
+function reduce(arr, fn, initial) {
+  if (arr.length === 0) {
+    return initial
+  }else {
+    var current = arr.shift();
+    initial = fn(initial, current)
+    return reduce(arr, fn, initial);
+  }
+}
+
+module.exports = reduce;
+
+
